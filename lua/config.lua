815dbe7f825aa2fed0e952d083ba2877481fd4a0
Pete Moore <pete@fightsticker.com>
2024-09-02 17:05:16 -0600

    move defaults into setup function

--- lua/config.lua
+++ lua/config.lua
@@ -1,25 +1,21 @@
---local actions = require("hercula.actions")
 local M = {}
 
-local defaults = {
-	mappings = {
-		i = {
-			--["<C-m>"] = { action = actions.mark },
-		},
-	},
-	dirs = {
-		vim.env.HOME,
-	},
-	scan = {
-		hidden = false,
-		depth = 5,
-		search_pattern = ".epub",
-	},
-}
-
-M.options = {}
-
 M.setup = function(opts)
+	local defaults = {
+		mappings = {
+			i = {
+				--["<C-m>"] = { action = actions.mark },
+			},
+		},
+		dirs = {
+			vim.env.HOME,
+		},
+		scan = {
+			hidden = false,
+			depth = 5,
+			search_pattern = ".epub",
+		},
+	}
 	opts = opts or {}
 	M.options = vim.tbl_deep_extend("force", defaults, opts)
 end
