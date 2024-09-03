443a1e9cdb4b4f5092c0e76186a3dace526fac9f
Pete Moore <pete@fightsticker.com>
2024-09-02 18:00:02 -0600

    add ext_config to setup function

--- lua/telescope/_extensions/hercula.lua
+++ lua/telescope/_extensions/hercula.lua
@@ -2,7 +2,7 @@ local telescope = require("telescope")
 local open = require("hercula").open
 local setup = require("config").setup
 return telescope.register_extension({
-	setup = setup,
+	setup = setup(ext_config),
 	exports = {
 		hercula = open,
 	},
